87b116214422deb8bed41d9ad8b5e0701ddf08cd
Pedro Souza <pedronicholas.souza@gmail.com>
2015-01-13 10:54:37 -0200

    make NSDate Extensions a Pod

--- Example/Tests/Tests.m
+++
@@ -1,9 +0,0 @@
-//
-//  NSDate-ExtensionsTests.m
-//  NSDate-ExtensionsTests
-//
-//  Created by Pedro Souza on 01/13/2015.
-//  Copyright (c) 2014 Pedro Souza. All rights reserved.
-//
-
-${TEST_EXAMPLE}
